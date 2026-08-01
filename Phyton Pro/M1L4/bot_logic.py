5b3bf21345861ad852e0a23c8ab28823769c4de6
juanjosevelaidessanabria-dotcom <juanjosevelaidessanabria@gmail.com>
2026-08-01 09:27:25 -0500

    Bot en M1L4

---
+++ Phyton Pro/M1L4/bot_logic.py
@@ -0,0 +1,26 @@
+"""Module providing a funtion to Discord bot."""
+import random
+
+def gen_pass(pass_length):
+    """Function creating a password."""
+    elements = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789+-/*!&$#?=@<>"
+    password = ""
+
+    for _ in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+def gen_emodji():
+    """Function sending a emoji."""
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    """Function flipping a coin."""
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "Cara"
+    else:
+        return "Sello"
